52d06ca683364489afb0501297b4ba376d66e2e6
FluffyTT <2831412058@qq.com>
2024-04-14 20:09:34 +0800

    add structure

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,6 @@ import ActivityDetails from "./Detial_page";
 import BookingPage from "./Booking_page";
 import Publish from "./publish/publish";
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
