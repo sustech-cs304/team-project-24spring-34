cc19a09f321fbd3058bca028958484c7b45591ee
LogicDX342 <52480482+LogicDX342@users.noreply.github.com>
2024-04-15 17:48:55 +0800

    Create react-test.yml

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Mainpage from './mainpage/Mainpage';
+
+function App() {
+  return (
+    <div className='App'>
+      <Mainpage />
+    </div>
+  );
+}
+
+export default App;
